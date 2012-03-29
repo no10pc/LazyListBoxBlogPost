2af52e17c64b95c4ef9b27aaa692345b05b45512
fu xuanyu <no10pc@gmail.com>
2012-03-29 17:24:44 +0800

    准备下班

---
+++ meituan/MyTransition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Phone.Controls;
+using System.Windows.Media.Animation;
+using System.Windows;
+
+
+namespace meituan
+{
+    public class MyTransition : TransitionElement
+    {
+        public const string StoryboardPropertyName = "Storyboard";
+
+        public Storyboard Storyboard
+        {
+            get { return (Storyboard)GetValue(StoryboardProperty); }
+            set { SetValue(StoryboardProperty, value); }
+        }
+
+        public static readonly DependencyProperty StoryboardProperty =
+            DependencyProperty.Register(StoryboardPropertyName, typeof(Storyboard), typeof(MyTransition), 
+            new PropertyMetadata(null));
+
+        public override ITransition GetTransition(System.Windows.UIElement element)
+        {
+            return new Transition(element, this.Storyboard);
+        }
+    }
+}
